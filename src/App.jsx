c2c892d11ac03a4fe6a45b59fbbcb5c830fb1661
Piotr Witkos <piotrek.witkos@gmail.com>
2021-03-28 13:28:08 +0200

    added pan to my location

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import {GoogleMap, Marker, useLoadScript} from '@react-google-maps/api'
 import mapStyles from './mapStyles';
+import {IoMdLocate} from 'react-icons/io';
+
+const libraries =  ["places"];
 
 const mapContainerStyle = {
   width: '300px',
@@ -19,12 +22,32 @@ const options = {
 }
 
 export function App(){
-  const  {isLoaded, loadError} = useLoadScript({googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY , libraries: ["places"]});
+  const  {isLoaded, loadError} = useLoadScript({googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY , libraries: libraries});
   
+  const mapRef = React.useRef();
+  const onMapLoad = React.useCallback( (map) => {mapRef.current = map;}, []);
+
+  const panTo = React.useCallback(({lat, lng}) => {
+    mapRef.current.panTo({lat,lng});
+    mapRef.current.setZoom(14);
+ }, []);
+
   if(loadError) return "Error loading maps";
   if(!isLoaded) return "Loading maps";
+
+
+
   return(<div>
-    <GoogleMap mapContainerStyle={mapContainerStyle} center={center} zoom={8} options={options}></GoogleMap>
+    <GoogleMap mapContainerStyle={mapContainerStyle} center={center} zoom={8} options={options} onLoad={onMapLoad}></GoogleMap>
+    <Locate panTo={panTo}/>
   </div>);
+}
+
+function Locate({panTo}){
+  return(<button onClick={()=>{
+    navigator.geolocation.getCurrentPosition((position)=>{
+      panTo({lat: position.coords.latitude, lng: position.coords.longitude})}, () => null)}}>
+    <IoMdLocate color='#ff2942' size='2em'/>
+  </button>)
 }
 
